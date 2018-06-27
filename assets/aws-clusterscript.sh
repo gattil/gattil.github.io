1d7ef12963bd5b9ce47f8e54d86bba9df58a96eb
Lorenzo Gatti <lorenzo.gatti.89@gmail.com>
2018-06-27 12:46:51 +0200

    Update aws-clusterscript.sh

--- assets/aws-clusterscript.sh
+++ assets/aws-clusterscript.sh
@@ -6,3 +6,9 @@ mkdir /mnt/efs
 mount -a
 chmod go+rw /mnt/efs
 chmod 777 /mnt/efs
+sudo apt-get update
+sudo apt-get install -y software-properties-common debconf-utils
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get update
+sudo echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+sudo apt-get install -y oracle-java8-installer
